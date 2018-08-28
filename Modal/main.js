fdde263b7de0af6ca168b8f067dca3419dec3dc0
Jordan Alexander Cruz - AlexCG Design <37056514+AlexCGDesign@users.noreply.github.com>
2018-08-28 16:59:42 -0500

    Add files via upload

---
+++ Modal/main.js
@@ -0,0 +1,19 @@
+let modal = document.getElementById('miModal');
+let flex = document.getElementById('flex');
+let abrir = document.getElementById('abrir');
+let cerrar = document.getElementById('close');
+
+abrir.addEventListener('click', function(){
+    modal.style.display = 'block';
+});
+
+cerrar.addEventListener('click', function(){
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+    console.log(e.target);
+    if(e.target == flex){
+        modal.style.display = 'none';
+    }
+});
